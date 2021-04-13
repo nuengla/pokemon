18341a2676bf7a528febc33ecddff76a42bcd7d6
nuengla <nuengla@hotmail.com>
2021-04-14 03:20:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import {getAllPokemon} from './components/pokemon'
+import Card from './components/Card'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setnextUrl] = useState('');
+  const [prevUrl, setprevUrl] = useState('');
+  const [loading, setloading] = useState(true);
+  const initialUrl ='https://pokeapi.co/api/v2/pokemon/?limit=10'
+
+
+  useEffect(() => {
+    async function fetchData(){
+      let response = await getAllPokemon(initialUrl);
+      setnextUrl(response.next);
+      setprevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setloading(false);
+    }
+    fetchData();
+  }, []);
+
+    const next = async () => {
+      setloading(true);
+      let data = await getAllPokemon(nextUrl)
+      await loadingPokemon (data.results)
+      setnextUrl(data.next);
+      setprevUrl(data.previous);
+      setloading(false);
+    }
+    const prev = async () => {
+      if (!prevUrl) return;
+      setloading(true);
+      let data = await getAllPokemon(prevUrl)
+      await loadingPokemon (data.results)
+      setnextUrl(data.next);
+      setprevUrl(data.previous);
+      setloading(false);
+    }
+
+  const loadingPokemon = async data => {
+    let _pokemonData = await Promise.all(
+      data.map(async pokemon => {
+        let pokemonRecord = await getAllPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+
+
+    setPokemonData(_pokemonData);
+  }
+
+
+
+
+
+   return(
+     <div>
+       {
+         loading ? <h1 className='load'>Loading...</h1> : (
+            <>
+           
+             <div className='btn'>
+                <button onClick={prev}>Prev</button>
+                <button onClick={next}>Next</button>
+              </div>
+              <div className='grid-container'>
+                {pokemonData.map((pokemon , i) => {
+                  return <Card key={i} pokemon={pokemon} />
+                })}
+
+              </div>
+              
+          
+              
+            </>
+         )
+       }
+     </div>
+   )
+       
+       
+/* test */
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+  
+
+  
+
 
 export default App;
+
+
+
